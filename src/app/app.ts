76a5eebaee343f4b0869c70e3260f66a68d2d851
safwen-code <safwendjebbi1234@gmail.com>
2026-04-01 22:37:09 +0100

    add face snap ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaceSnap } from './face-snap/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FaceSnap],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('snapface');
-}
+export class App {}
---
+++ src/app/face-snap/face-snap.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  imports: [],
+  templateUrl: './face-snap.html',
+  styleUrl: './face-snap.scss',
+})
+export class FaceSnap {}
