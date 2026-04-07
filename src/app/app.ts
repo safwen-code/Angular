9145040a7032644f5b7cf6eb882e22132a783b49
safwen-code <safwendjebbi1234@gmail.com>
2026-04-07 17:19:36 +0100

    work with class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from './face-snap/face-snap';
-
+import { FaceSnapclass } from './models/face-snap';
 @Component({
   selector: 'app-root',
   imports: [FaceSnap],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App implements OnInit {
+  mySnap!: FaceSnapclass;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnapclass(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      10
+    );
+  }
+}
