77b4b522ecf5684aaab5278aaadbb47a98e50312
safwen-code <safwendjebbi1234@gmail.com>
2026-04-07 16:19:37 +0100

    update user event

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -12,14 +12,29 @@ export class FaceSnap implements OnInit {
   createdDate!: Date;
   snaps!: number;
   image!: string;
+  textsnap!: string;
+  hassnaped!: boolean;
+
   ngOnInit(): void {
     this.name = 'Archibald';
     this.description = 'Mon meilleur ami depuis tout petit !';
     this.createdDate = new Date();
     this.snaps = 0;
     this.image = 'assets/camera.jpg';
+    this.textsnap = 'Oh Snap !';
+    this.hassnaped = false;
   }
-  onAddSnap() {
+  Snap() {
+    return this.hassnaped ? this.removeSnap() : this.onSnap();
+  }
+  removeSnap() {
+    this.snaps--;
+    this.hassnaped = false;
+    this.textsnap = 'Oh Snap !';
+  }
+  onSnap() {
     this.snaps++;
+    this.hassnaped = true;
+    this.textsnap = 'Oops, un snap en moins !';
   }
 }
