599ccad4302037d3f1f97ea11605807983af3ce4
safwen-code <safwendjebbi1234@gmail.com>
2026-04-09 00:52:22 +0100

    data sturctur ui

---
+++ src/app/face-snap-list/face-snap-list.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FaceSnapclass } from '../models/face-snap';
+import { FaceSnap } from '../face-snap/face-snap';
+
+@Component({
+  selector: 'app-face-snap-list',
+  imports: [FaceSnap],
+  templateUrl: './face-snap-list.html',
+  styleUrl: './face-snap-list.scss',
+})
+export class FaceSnapList implements OnInit {
+  FaceSnaps!: FaceSnapclass[];
+
+  ngOnInit(): void {
+    this.FaceSnaps = [
+      new FaceSnapclass(
+        'Archibald',
+        'Mon meilleur ami depuis toujours !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnapclass(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnées.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        new Date(),
+        6
+      ),
+      new FaceSnapclass(
+        'Un bon repas',
+        "Mmmh que c'est bon !",
+        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        new Date(),
+        156
+      ),
+    ];
+    this.FaceSnaps[1].setLocation('à la montagne');
+  }
+}
